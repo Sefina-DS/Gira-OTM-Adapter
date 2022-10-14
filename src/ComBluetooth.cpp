478eab67e52ecfe522b9b4db4d10ce33ebc12d29
Sefina-DS <35521885+Sefina-DS@users.noreply.github.com>
2022-10-14 22:24:11 +0200

    Bluetooth diagnose + namensbearbeitung

--- src/ComBluetooth.cpp
+++ src/ComBluetooth.cpp
@@ -6,7 +6,7 @@ BLEScan *pBLEScan;
 void bluetooth_config()
 {
     bluetooth.konfiguriert = true;
-    for (int i = 0; i < bluetooth.anzahl; i++)
+    for (int i = 0; i < bluetooth.mac_size; i++)
     {
         bluetooth.empfang[i] = 0;
     }
@@ -24,7 +24,9 @@ void bluetooth_config()
         Serial.print("Adresse ");
         Serial.print(i);
         Serial.print(" : ");
-        Serial.println(bluetooth.adressen[i]);
+        Serial.print(bluetooth.mac_adress[i]);
+        Serial.print("   |   ");
+        Serial.println(bluetooth.mac_name[i]);
     }
     
     //Serial.println(bluetooth.adressen[0]);
@@ -42,13 +44,14 @@ void bluetooth_scan()
     {
         String gefunden = foundDevices.getDevice(j).getAddress().toString().c_str();
         int empfang = foundDevices.getDevice(j).getRSSI() + 100;
-        for (i = 0; i < bluetooth.anzahl; i++)
+        for (i = 0; i < bluetooth.mac_size; i++)
         {
-            String temp = bluetooth.adressen[i];
-            if ( gefunden == temp)
+            String temp_adress = bluetooth.mac_adress[i];
+            String temp_name = bluetooth.mac_name[i];
+            if ( gefunden == temp_adress)
             {
                 bluetooth.empfang[i] = empfang;
-                mqtt_publish(mqtt.topic_base + "/" + mqtt.topic_define + "/" + extension + ext_bluetooth + temp, String(empfang));
+                mqtt_publish(mqtt.topic_base + "/" + mqtt.topic_define + "/" + extension + ext_bluetooth + temp_name, String(empfang));
             }
         }
         
@@ -65,12 +68,12 @@ void bluetooth_scan()
 
     if (number_circle == 10)
     {
-        for (i = 0; i < bluetooth.anzahl; i++)
+        for (i = 0; i < bluetooth.mac_size; i++)
         {
-            String temp = bluetooth.adressen[i];
+            String temp_name = bluetooth.mac_name[i];
             if (bluetooth.empfang[i] == 0)
             {
-                mqtt_publish(mqtt.topic_base + "/" + mqtt.topic_define + "/" + extension + ext_bluetooth + temp, String(0));
+                mqtt_publish(mqtt.topic_base + "/" + mqtt.topic_define + "/" + extension + ext_bluetooth + temp_name, String(0));
             }
             else
             {
@@ -133,6 +136,8 @@ void load_conf_bluetooth(StaticJsonDocument<1024> doc)
     Serial.println("... Bluetooth- Variablen ...");
     
     bluetooth.aktiv = doc["bluetooth"] | false;
+    bluetooth.memory_file = doc["bluetooth"] | "589ec621333b(G-Tag David) 589ec61a30ca(G-Tag Janin) 589ec61a30dd(G-Tag Florentine) 589ec6204371(G-Tag Hundeleine) 589ec62043c6(G-Tag Cedrik)";
+    bluetooth_mac_diagnose(bluetooth.memory_file);
 }
 
 StaticJsonDocument<1024> safe_conf_bluetooth(StaticJsonDocument<1024> doc)
@@ -142,4 +147,118 @@ StaticJsonDocument<1024> safe_conf_bluetooth(StaticJsonDocument<1024> doc)
     doc["bluetooth"] = bluetooth.aktiv;
 
     return doc;
+}
+
+void bluetooth_mac_diagnose(String msg)
+{
+    String temp_name = "";
+    char temp_char;
+
+    bluetooth.temp_mac_a = 0;
+    bluetooth.temp_mac_b = 0;
+    bluetooth.temp_mac_klein = "";
+    bluetooth.temp_mac_gross = ""; 
+    bluetooth.diagnose_mac = true;  
+    bluetooth.memory_file = ""; 
+    bluetooth.mac_size = 0;
+
+    for (int i = 0; msg[i] != 0; i++)
+    {
+        if (msg[i] == 40) // = (
+        {
+            bluetooth.diagnose_mac = false;
+            bluetooth.memory_file += msg[i];
+        }
+        if (bluetooth.diagnose_mac == true)
+        {
+            // ZAHLEN
+            if (msg[i] > 47 && msg[i] < 58)
+            {
+                if (bluetooth.temp_mac_a == 2 && bluetooth.temp_mac_b == 5)
+                {
+                    bluetooth_mac();
+                }
+                bluetooth.temp_mac_klein += msg[i];
+                bluetooth.temp_mac_gross += msg[i];
+                bluetooth.memory_file += msg[i];
+                bluetooth.temp_mac_a ++;    
+            }
+            // BUCHSTABEN KLEIN
+            if (msg[i] > 96 && msg[i] < 123)
+            {
+                if (bluetooth.temp_mac_a == 2 && bluetooth.temp_mac_b == 5)
+                {
+                    bluetooth_mac();
+                }
+                temp_char = msg[i] - 32;
+                bluetooth.temp_mac_klein += msg[i];
+                bluetooth.temp_mac_gross += temp_char;
+                bluetooth.memory_file += msg[i];
+                bluetooth.temp_mac_a ++;    
+            }
+            // BUCHSTABEN GROß
+            if (msg[i] > 64 && msg[i] < 91)
+            {
+                if (bluetooth.temp_mac_a == 2 && bluetooth.temp_mac_b == 5)
+                {
+                    bluetooth_mac();
+                }
+                temp_char = msg[i] + 32;
+                bluetooth.temp_mac_klein += temp_char;
+                bluetooth.temp_mac_gross += msg[i];
+                bluetooth.memory_file += temp_char;
+                bluetooth.temp_mac_a ++;    
+            }
+            // -> : <- WERDEN EINGEFÜGT
+            if (bluetooth.temp_mac_a == 2 && bluetooth.temp_mac_b != 5)
+            {
+                bluetooth.temp_mac_gross += ":";
+                bluetooth.temp_mac_klein += ":";
+                bluetooth.temp_mac_a = 0;
+                bluetooth.temp_mac_b ++;
+            }
+        }
+        else
+        {
+            if (msg[i] == 41)
+            {
+                bluetooth.memory_file += ")";
+                bluetooth.temp_mac_gross = temp_name + " (" + bluetooth.temp_mac_gross + ")";
+                temp_name = "";
+                bluetooth_mac();
+            }
+            else
+            {
+                if (msg[i] != 40)
+                {
+                    temp_name += msg[i];
+                }
+                bluetooth.memory_file += msg[i];
+            }
+        }
+    }
+    if (bluetooth.temp_mac_b == 5)
+    {
+        bluetooth_mac();
+    }
+
+}
+
+void bluetooth_mac()
+{
+    for (int j = 0; bluetooth.temp_mac_gross[j] != 0 ; j++)
+    {
+        bluetooth.mac_name[bluetooth.mac_size][j] = bluetooth.temp_mac_gross[j];
+    }
+    for (int k = 0; bluetooth.temp_mac_klein[k] != 0 ; k++)
+    {
+        bluetooth.mac_adress[bluetooth.mac_size][k] = bluetooth.temp_mac_klein[k];
+    }
+    bluetooth.memory_file += " ";
+    bluetooth.diagnose_mac = true;
+    bluetooth.temp_mac_a = 0;
+    bluetooth.temp_mac_b = 0;
+    bluetooth.temp_mac_klein = "";
+    bluetooth.temp_mac_gross = "";
+    bluetooth.mac_size ++;
 }
